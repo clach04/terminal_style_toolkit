22aea3b0a1ef327a17d0821c4aefbb32abbcbc20
clach04 <clach04@gmail.com>
2023-11-18 09:16:13 -0800

    Putty tool dump color info and find duplicates

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Scripts / tools / config for terminal styling and colors / colours
       * Unknown license, many different versions of this script online
   * show_colors2.bash my version derived from show_colors.bash
 
+## Putty
+
+  * python_windows_registry_putty_colors.py - dumps session names and colors (only)
+    along with simple show sessions that use the exact same same-color scheme feature
---
+++ python_windows_registry_putty_colors.py
@@ -0,0 +1,78 @@
+import errno
+import json
+try:
+    import winreg
+except ImportError:
+    import _winreg as winreg
+
+
+try:
+    FileNotFoundError
+except NameError:
+    # py2
+    FileNotFoundError = OSError  # FIXME review, use variable for tuple of exceptions instead?
+
+access_type = winreg.KEY_ALL_ACCESS  # needs admin, or get; WindowsError: [Error 5] Access is denied / PermissionError: [WinError 5] Access is denied 
+access_type = winreg.KEY_READ 
+
+
+key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, "SOFTWARE\\SimonTatham\\PuTTY\\Sessions\\", 0, access_type)
+sessions = {}
+colors_to_session_names = {}
+for i in range(0, winreg.QueryInfoKey(key)[0]):
+    putty_session_name = winreg.EnumKey(key, i)  # putty session name
+    print('\t' + putty_session_name)
+    skey = winreg.OpenKey(key, putty_session_name)
+    try:
+        """
+        try:
+            key_name = 'ANSIColour'
+            print('\t\t' + key_name + ' ' , winreg.QueryValueEx(skey, key_name)[0])
+        except (OSError, FileNotFoundError) as e:
+            if e.errno == errno.ENOENT:
+                # DisplayName doesn't exist in this skey
+                pass
+        try:
+            key_name = 'BoldAsColour'  # a handful of my sessions have different values for this
+            print('\t\t' + key_name + ' ' , winreg.QueryValueEx(skey, key_name)[0])
+        except (OSError, FileNotFoundError) as e:
+            if e.errno == errno.ENOENT:
+                # DisplayName doesn't exist in this skey
+                pass
+        try:
+            key_name = 'UseSystemColours'
+            print('\t\t' + key_name + ' ' , winreg.QueryValueEx(skey, key_name)[0])
+        except (OSError, FileNotFoundError) as e:
+            if e.errno == errno.ENOENT:
+                # DisplayName doesn't exist in this skey
+                pass
+        try:
+            key_name = 'Xterm256Colour'
+            print('\t\t' + key_name + ' ' , winreg.QueryValueEx(skey, key_name)[0])
+        except (OSError, FileNotFoundError) as e:
+            if e.errno == errno.ENOENT:
+                # DisplayName doesn't exist in this skey
+                pass
+        """
+        colors = {}
+        for j in range(21+1):
+            try:
+                key_name = 'Colour%d' % j
+                colors[key_name] = winreg.QueryValueEx(skey, key_name)[0]
+                print('\t\t' + key_name + ' ' + winreg.QueryValueEx(skey, key_name)[0])
+            except (OSError, FileNotFoundError) as e:
+                if e.errno == errno.ENOENT:
+                    # DisplayName doesn't exist in this skey
+                    pass
+        sessions[putty_session_name] = colors
+        color_key = json.dumps(colors)
+        tmp_list = colors_to_session_names.get(color_key, [])
+        tmp_list.append(putty_session_name)
+        colors_to_session_names[color_key] = tmp_list
+    finally:
+        skey.Close()
+
+# show sessions that have identical color schemes
+for x in colors_to_session_names:
+    print(colors_to_session_names[x])
+# Showing similar would require diffing each scheme and having a thresh hold for differences in color / and/or levingstien distance (etc.) for fuzzy match
