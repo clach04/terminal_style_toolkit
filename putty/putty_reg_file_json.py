ed5169f3544983b923a80a474cf1a77fc261d5ec
clach04 <clach04@gmail.com>
2024-01-18 20:36:25 -0800

    Dump sorted, detailed reg entries when converting reg file
    Exe bit set for al python scripts

--- putty/putty_reg_file_json.py
+++ putty/putty_reg_file_json.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+#
+
 # Clone of putty_reg_file_to_sorted.py - todo refactor and share code
 import json
 import re
