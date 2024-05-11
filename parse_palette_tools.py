926265fc63c69aa977cecd856cadea696250425b
clach04 <clach04@gmail.com>
2024-05-11 13:45:33 -0700

    Handle additional GIMP palette format

--- parse_palette_tools.py
+++ parse_palette_tools.py
@@ -25,6 +25,7 @@ def open_parse_gimp_palette_gpl_file(filename):
     color_names = {}
     color_names_list = []  # ordered as found in GIMP Palette gpl file
     found_colors = False
+    color_entry_style = 'UNKNOWN'
     for line in f:
         line = line.strip()
         if not line:
@@ -33,15 +34,40 @@ def open_parse_gimp_palette_gpl_file(filename):
         if line == '#':
             found_colors = True
             continue
+        if not found_colors and line.startswith('#Colors:'):
+            color_entry_style = 'DECIMAL_RBG+HEX'
+            found_colors = True
+            continue
+        if not found_colors and line.startswith('Name:'):
+            continue
+        if not found_colors and line.startswith('Columns:'):
+            expected_column_count = int(line.split(':')[1])  # expecting 3....
+            if expected_column_count == 3:
+                color_entry_style = 'NAMED'  # 3 for RGB and + 1 for name
+            else:
+                raise NotImplementedError('%d columns for colors' % expected_column_count)
+            continue
         if found_colors:
             #print('LINE: %r' % line)
             #print('LINE: %r' % line.split())
-            colors, color_name = line.split('\t')
-            r, g, b = map(int, colors.split())
-            #print(color_name, r, g, b)
-            if color_name in color_names:
-                color_name += '_%02x%02x%02x' % (r, g, b)
-            color_names[color_name] = (r, g, b)
+            if color_entry_style == 'NAMED':
+                colors, color_name = line.split('\t')
+                r, g, b = map(int, colors.split())
+                #print(color_name, r, g, b)
+                if color_name in color_names:
+                    color_name += '_%02x%02x%02x' % (r, g, b)
+                color_names[color_name] = (r, g, b)
+            elif color_entry_style == 'DECIMAL_RBG+HEX':
+                color_name = 'blank'
+                r, g, b, hex_rgb = line.split('\t')
+                colors = (r, g, b)
+                r, g, b = map(int, colors)
+                #print(color_name, r, g, b)
+                if color_name in color_names:
+                    color_name += '_%02x%02x%02x' % (r, g, b)
+                color_names[color_name] = (r, g, b)
+            else:
+                raise NotImplementedError('Entry style %s' % color_entry_style)
             color_names_list.append(color_name)
 
     f.close()
