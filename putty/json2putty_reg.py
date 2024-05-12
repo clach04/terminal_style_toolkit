4a5db4ce197fd176d1219987308ccd2323058149
clach04 <clach04@gmail.com>
2024-05-11 17:03:55 -0700

    New tool json2putty_reg - Render Putty registry file using a json file as input

---
+++ putty/json2putty_reg.py
@@ -0,0 +1,40 @@
+
+#!/usr/bin/env python
+# -*- coding: us-ascii -*-
+# vim:ts=4:sw=4:softtabstop=4:smarttab:expandtab
+#
+"""Json to Windows registry session file for Putty
+
+Expects json file to contain same values as Putty registry, i.e.:
+
+    {
+        "scheme-name" : "Optional name - can be omitted",
+        "Colour0" : "187,187,187",
+        ...
+        "Colour21" : "238,238,236"
+    }
+"""
+
+import os
+import sys
+import json
+
+import putty_colors_render_template
+
+
+in_filename = sys.argv[1]
+f = open(in_filename, 'r')
+x = f.read()
+f.close()
+
+putty_color_dict = json.loads(x)
+putty_color_dict['scheme-name'] = putty_color_dict.get('scheme-name', 'UNKNOWN')
+session_name = putty_color_dict['scheme-name']
+
+reg_entries = putty_colors_render_template.render_template(putty_color_dict)
+#filename = os.path.join(output_dir, session_name) + '_sorted.reg'
+filename = session_name + '_sorted.reg'
+f = open(filename, 'w')
+f.write(reg_entries)
+f.close()
+print('Wrote to %s' % filename)
