2fcbb5502dd6693c7cfe7f43279cb1acdea10887
clach04 <clach04@gmail.com>
2025-08-03 10:36:21 -0700

    Include theme name in console_view_tstk preview

--- putty/console_view_tstk.py
+++ putty/console_view_tstk.py
@@ -38,7 +38,7 @@ def main(argv=None):
         color_dict = json.loads(x)
 
         putty_colors_render_template.print_colors_terminal(color_dict)
-        print("  %s" % (filename,))  # TODO theme name
+        print("  %s : %s" % (filename, color_dict.get("scheme-name", "MISSING NAME")))
 
     print('%d files' % len(filenames))
 
